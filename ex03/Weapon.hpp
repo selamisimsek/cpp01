0c7d62eab4a2f75073e4d08b62f6c4f4888df7f6
Selami Ali Şimşek <sesimsek@k1m09s03.42istanbul.com.tr>
2025-10-23 20:50:54 +0300

    ex03 finitoooooo

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: sesimsek <sesimsek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 20:37:41 by sesimsek          #+#    #+#             */
-/*   Updated: 2025/10/21 20:55:29 by sesimsek         ###   ########.fr       */
+/*   Updated: 2025/10/23 20:49:44 by sesimsek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ class Weapon{
 	private:
 		std::string type;		
 	public:
-		Weapon(std::string type);
-		void setType(std::string newType);
+		Weapon(const std::string type);
+		void setType(const std::string newType);
 		const std::string getType() const;
 };
 
